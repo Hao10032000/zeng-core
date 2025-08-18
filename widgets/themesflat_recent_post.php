6789ba6d44c9a4edc5f8926f594bfaaa8bacd71f
cuong <huycuongytam@gmail.com>
2025-08-18 22:20:13 +0700

    update

--- widgets/themesflat_recent_post.php
+++ widgets/themesflat_recent_post.php
@@ -17,7 +17,11 @@ class Themesflat_Recent_Post extends WP_Widget {
             'title' 	=> 'Recent News', 
             'category'  => '',
             'ids'  => '',
-            'count' 	=> 4          
+            'count' 	=> 4,
+            'show_thumbnail' => true,
+            'show_content' => false,
+            'show_date' => true,
+            'show_author' => true           
         );
         parent::__construct(
             'widget_recent_post',
@@ -51,76 +55,53 @@ class Themesflat_Recent_Post extends WP_Widget {
             $query_args['post__in'] = explode(",",$ids);
              $query_args['orderby'] = 'post__in';
         }
-        global $wp_query;
-$current_posts_ids = wp_list_pluck( $wp_query->posts, 'ID' );
-$query_args['post__not_in'] = $current_posts_ids;
         $flat_post = new WP_Query( $query_args );
-        $classes[] = 'recent-news wrap-post';
+        $classes[] = 'recent-news';
+        $classes[] = $show_thumbnail != 1 ? 'no-thumbnail' : '';
         echo wp_kses_post( $before_widget );
-		if ( !empty($title) ) { echo wp_kses_post($before_title).esc_html($title).wp_kses_post($after_title); } ?>
-<ul class="<?php echo esc_attr(implode(' ', $classes)) ;?> clearfix">
-    <?php if ( $flat_post->have_posts() ) : ?>
-    <?php $count = 1; while ( $flat_post->have_posts() ) : $flat_post->the_post(); ?>
-    <li class="feature-post-item style-default hover-image-translate">
-        <?php if ( $count == 1) : ?>
-        <div class="img-style mb_24">
-            <a href="<?php the_permalink(); ?>">
-                <?php 
-                                the_post_thumbnail( 'themesflat-blog-style1' );
-                            ?>
-            </a>
-            <?php
-
-                $categories = get_the_category();
-
-                if (!empty($categories)) {
-                    echo '<span class="post-category">';
-                    foreach ($categories as $category) {
-                        $category_link = esc_url(get_category_link($category->term_id));
-                        $category_name = esc_html($category->name);
-
-                        echo '<a href="' . $category_link . '" class="tag categories text-caption-2 text_white">' . $category_name . '</a> ';
-                    }
-                    echo '</span>';
-                }
-
-                echo '<div class="tag time text-caption-2 text_white"><i class="icon-Timer"></i> ' . get_reading_time() . '</div>';
-        ?>
-        </div>
-        <?php endif; ?>
-        <div class="content">
-
-            <ul class="meta-feature fw-7 d-flex text-caption-2 mb_11">
-                <li class="author">
-                    <span class="by"><?php esc_html_e( 'by', 'zeng' ); ?></span>
-                    <?php
-        $author_id = get_post_field('post_author', get_the_ID());
-        printf(
-            ' <a class="link author-name" href="%s" title="%s" rel="author">%s</a>',
-            esc_url( get_author_posts_url( $author_id ) ),
-            esc_attr( sprintf( __( 'View all posts by %s', 'zeng' ), get_user_full_name($author_id) )),
-            get_user_full_name($author_id)
-        );
-        ?>
-                </li>
-                <li class="date">
-                    <?php echo get_the_date(); ?>
-                </li>
-            </ul>
-
-
-
-            <?php the_title( sprintf( '<h6 class="title"><a href="%s" class="link" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h6>' ); ?>
-
-        </div><!-- /.text -->
-    </li>
-    <?php $count++; endwhile; ?>
-    <?php wp_reset_postdata(); ?>
-    <?php else : ?>
-    <?php printf( '<li>%s</li>',esc_html__('Oops, category not found.', 'themesflat' )); ?>
-    <?php endif; ?>
-</ul>
-<?php echo wp_kses_post( $after_widget );
+		if ( !empty($title) ) { echo wp_kses_post($before_title).esc_html($title).wp_kses_post($after_title); } ?>		
+        <ul class="<?php echo esc_attr(implode(' ', $classes)) ;?> clearfix">  
+		<?php if ( $flat_post->have_posts() ) : ?>
+			<?php while ( $flat_post->have_posts() ) : $flat_post->the_post(); ?>
+				<li class="clearfix">
+                    <?php if ( has_post_thumbnail() && $show_thumbnail == 1) : ?>
+                    <div class="thumb">
+                        <span class="overlay-pop"></span>
+                        <a href="<?php the_permalink(); ?>">
+                        <?php 
+                        the_post_thumbnail( 'thumbnail' );
+                        ?>
+                        </a>
+                    </div>
+                    <?php endif; ?>                       
+                    <div class="text">     
+                                                                   
+                        <?php if ( $show_content ) : ?>
+                        <p class="desc"><?php echo wp_trim_words( get_the_content(), 8, '...' ); ?></p>
+                        <?php endif; ?>
+                        <?php the_title( sprintf( '<h6><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h6>' ); ?>   
+                        <div class="wrap-meta">
+                            <?php if ( $show_author ) : ?>
+                            <div class="post-author">
+                                <?php esc_html_e( 'by ', 'themesflat' ); ?>
+                                <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+                                    <?php the_author(); ?>
+                                </a>
+                            </div> 
+                            <?php endif; ?>     
+                            <?php if ( $show_date ) : ?>
+                            <time class="post-date date updated" datetime="<?php esc_attr(the_time( 'c' )); ?>"><?php the_time( ' d F Y' ); ?></time>
+                            <?php endif; ?>                   
+                        </div>                     
+                    </div><!-- /.text -->                        
+			    </li>
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+			<?php else : ?>  
+            <?php printf( '<li>%s</li>',esc_html__('Oops, category not found.', 'themesflat' )); ?>
+		<?php endif; ?>        
+        </ul>		
+		<?php echo wp_kses_post( $after_widget );
     }
 
     /**
@@ -131,6 +112,10 @@ $query_args['post__not_in'] = $current_posts_ids;
         $instance['title']      = strip_tags( $new_instance['title'] );
         $instance['ids']      = ( $new_instance['ids'] );
         $instance['count']      =  intval($new_instance['count']);
+        $instance['show_thumbnail']     =  (bool) $new_instance['show_thumbnail'] ;
+        $instance['show_date']     = isset( $new_instance['show_date'] ) ? (bool) $new_instance['show_date'] : false;
+        $instance['show_content']     = isset( $new_instance['show_content'] ) ? (bool) $new_instance['show_content'] : false;
+        $instance['show_author']     = isset( $new_instance['show_author'] ) ? (bool) $new_instance['show_author'] : false;       
         $instance['category']           = array_filter( $new_instance['category'] );        
         return $instance;
     }
@@ -140,47 +125,59 @@ $query_args['post__not_in'] = $current_posts_ids;
      */
     function form( $instance ) {
         $instance = wp_parse_args( $instance, $this->defaults );
+        $show_content = $instance['show_content'] ? "checked" : "";
+        $show_content   = isset( $instance['show_content'] ) ? (bool) $instance['show_content'] : false;
+        $show_thumbnail   = isset( $instance['show_thumbnail'] ) ? (bool) $instance['show_thumbnail'] : true;
+        $show_date = $instance['show_date'] ? "checked" : "";
+        $show_date   = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+        $show_author = $instance['show_author'] ? "checked" : "";
+        $show_author   = isset( $instance['show_author'] ) ? (bool) $instance['show_author'] : false;
+
         if (empty($instance['category'])) {                    
             $instance['category'] = array("1");
         }
         ?>
-<p>
-    <label
-        for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'themesflat' ); ?></label>
-    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
-        name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text"
-        value="<?php echo esc_attr( $instance['title'] ); ?>">
-</p>
-<p>
-    <label
-        for="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"><?php esc_html_e( 'Select Category:', 'themesflat' ); ?></label>
-    <select class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"
-        name="<?php echo esc_attr( $this->get_field_name( 'category' ) ); ?>[]">
-        <option value="" <?php selected( empty( $instance['category'] ) ); ?>>
-            <?php esc_html_e( 'All', 'themesflat' ); ?></option>
-        <?php               
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'themesflat' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"><?php esc_html_e( 'Select Category:', 'themesflat' ); ?></label>
+            <select class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'category' ) ); ?>[]">
+                <option value=""<?php selected( empty( $instance['category'] ) ); ?>><?php esc_html_e( 'All', 'themesflat' ); ?></option>
+                <?php               
                 $categories = get_categories();
                 foreach ($categories as $category) {
                     printf('<option value="%1$s" %4$s>%2$s (%3$s)</option>', esc_attr($category->term_id), esc_attr($category->name), esc_attr($category->count), (in_array($category->term_id, $instance['category'] )) ? 'selected="selected"' : '');
                 }               
                 ?>
-    </select>
-</p>
-<p>
-    <label
-        for="<?php echo esc_attr( $this->get_field_id( 'ids' ) ); ?>"><?php esc_html_e( 'Get Post by IDS EX:1,2,3', 'themesflat' ); ?></label>
-    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'ids' ) ); ?>"
-        name="<?php echo esc_attr( $this->get_field_name( 'ids' ) ); ?>" type="text"
-        value="<?php echo esc_attr( $instance['ids'] ); ?>">
-</p>
-<p>
-    <label
-        for="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>"><?php esc_html_e( 'Count:', 'themesflat' ); ?></label>
-    <input class="widefat" type="number" id="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>"
-        name="<?php echo esc_attr( $this->get_field_name( 'count' ) ); ?>"
-        value="<?php echo esc_attr( $instance['count'] ); ?>">
-</p>
-<?php
+            </select>
+        </p>
+      <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'ids' ) ); ?>"><?php esc_html_e( 'Get Post by IDS EX:1,2,3', 'themesflat' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'ids' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'ids' ) ); ?>" type="text" value="<?php echo esc_attr( $instance['ids'] ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>"><?php esc_html_e( 'Count:', 'themesflat' ); ?></label>
+            <input class="widefat" type="number" id="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'count' ) ); ?>" value="<?php echo esc_attr( $instance['count'] ); ?>">
+        </p>
+        <p>
+            <input class="checkbox" type="checkbox" <?php checked( $show_thumbnail ); ?> id="<?php echo esc_attr( $this->get_field_id( 'show_thumbnail' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_thumbnail' ) ); ?>" />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'show_thumbnail' ) ); ?>"><?php esc_html_e( 'Show Thumbnail ?', 'themesflat' ) ?></label>
+        </p>  
+        <p>
+            <input class="checkbox" type="checkbox" <?php checked( $show_content ); ?> id="<?php echo esc_attr( $this->get_field_id( 'show_content' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_content' ) ); ?>" />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'show_content' ) ); ?>"><?php esc_html_e( 'Show Content ?', 'themesflat' ) ?></label>
+        </p>       
+        <p>
+            <input class="checkbox" type="checkbox" <?php checked( $show_date ); ?> id="<?php echo esc_attr( $this->get_field_id( 'show_date' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_date' ) ); ?>" />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'show_date' ) ); ?>"><?php esc_html_e( 'Show Date ?', 'themesflat' ); ?></label>
+        </p>
+        <p>
+            <input class="checkbox" type="checkbox" <?php checked( $show_author ); ?> id="<?php echo esc_attr( $this->get_field_id( 'show_author' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_author' ) ); ?>" />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'show_author' ) ); ?>"><?php esc_html_e( 'Show Author ?', 'themesflat' ); ?></label>
+        </p>
+    <?php
     }
 }
 
